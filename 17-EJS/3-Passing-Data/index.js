1cdba7d8ea54c04fe92a92cbe08cdb10de7bbaa6
Nikhil Bhaladhare <nikbhaladhare2104@gmail.com>
2024-05-06 21:10:04 +0530

    Add files via upload

--- 17-EJS/3-Passing-Data/index.js
+++ 17-EJS/3-Passing-Data/index.js
@@ -1,1 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  var data = req.body["fName"] + req.body["lName"];
+  var dataLength = data.length;
+  res.render("index.ejs", {
+    numOfLetters : dataLength
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
